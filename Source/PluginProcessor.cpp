a1a8a5408942ee5c821cd43709c560ca7d7cdf18
Matthew Rice <mattricesound@gmail.com>
2023-03-21 19:47:06 +0000

    Clean 2

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -2,44 +2,51 @@
 #include "PluginEditor.h"
 
 Text2SampleAudioProcessor::Text2SampleAudioProcessor()
-        : AudioProcessor(BusesProperties().withOutput("Output", juce::AudioChannelSet::stereo(), true)),
-          _parameters(*this, nullptr, "PARAMS", createParameterLayout()) {
-    for (int i = 0; i < numSounds; i++) {
+  : AudioProcessor(BusesProperties().withOutput("Output", juce::AudioChannelSet::stereo(), true)),
+    _parameters(*this, nullptr, "PARAMS", createParameterLayout())
+{
+    for (int i = 0; i < numSounds; i++)
+    {
         auto sound = new SoundWithParameters(baseMidiNote + i);
         _sounds.push_back(sound);
         _synth.addSound(sound);
     }
 
-    for (int i = 0; i < numVoices; i++) {
+    for (int i = 0; i < numVoices; i++)
+    {
         auto voice = new Voice();
         _voices.push_back(voice);
         _synth.addVoice(voice);
     }
 }
 
-Text2SampleAudioProcessor::~Text2SampleAudioProcessor() {
+Text2SampleAudioProcessor::~Text2SampleAudioProcessor()
+{
 }
 
-bool Text2SampleAudioProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const {
+bool Text2SampleAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
+{
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
     return true;
 }
 
-void Text2SampleAudioProcessor::prepareToPlay(double sampleRate, int) {
+void Text2SampleAudioProcessor::prepareToPlay(double sampleRate, int)
+{
     // Unused functionality but the Synthesiser class requires a real sample rate value
     _synth.setCurrentPlaybackSampleRate(sampleRate);
 
     // Sample rates are actually handled by the Sound class
     for (int i = 0; i < _synth.getNumSounds(); i++)
-        if (auto s = dynamic_cast<Sound *>(_synth.getSound(i).get()))
+        if (auto s = dynamic_cast<Sound*>(_synth.getSound(i).get()))
             s->setSampleRate(sampleRate);
 
     _midiState.reset();
 }
 
-void Text2SampleAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer, juce::MidiBuffer &midi) {
+void Text2SampleAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midi)
+{
     juce::ScopedNoDenormals noDenormals;
 
     _midiState.processNextMidiBuffer(midi, 0, buffer.getNumSamples(), true);
@@ -47,48 +54,58 @@ void Text2SampleAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer, j
     buffer.clear();
     _synth.renderNextBlock(buffer, midi, 0, buffer.getNumSamples());
 
-    Limiter limit{buffer};
+    Limiter limit{ buffer };
 
     midi.clear();
 }
 
-void Text2SampleAudioProcessor::releaseResources() {
+void Text2SampleAudioProcessor::releaseResources()
+{
 }
 
-void Text2SampleAudioProcessor::getStateInformation(juce::MemoryBlock &) {
+void Text2SampleAudioProcessor::getStateInformation (juce::MemoryBlock&)
+{
 }
 
-void Text2SampleAudioProcessor::setStateInformation(const void *, int) {
+void Text2SampleAudioProcessor::setStateInformation (const void*, int)
+{
 }
 
-bool Text2SampleAudioProcessor::hasEditor() const {
+bool Text2SampleAudioProcessor::hasEditor() const
+{
     return true;
 }
 
-juce::AudioProcessorEditor *Text2SampleAudioProcessor::createEditor() {
-    return new Text2SampleAudioProcessorEditor(*this);
+juce::AudioProcessorEditor* Text2SampleAudioProcessor::createEditor()
+{
+    return new Text2SampleAudioProcessorEditor (*this);
 }
 
-juce::MidiKeyboardState &Text2SampleAudioProcessor::getMidiKeyboardState() {
+juce::MidiKeyboardState& Text2SampleAudioProcessor::getMidiKeyboardState()
+{
     return _midiState;
 }
 
-SoundWithParameters *Text2SampleAudioProcessor::getSound(int soundIndex) {
+SoundWithParameters* Text2SampleAudioProcessor::getSound(int soundIndex)
+{
     jassert(juce::isPositiveAndBelow(soundIndex, _sounds.size()));
     return _sounds[soundIndex];
 }
 
-void Text2SampleAudioProcessor::loadSample(int soundIndex, Sample::Ptr sample) {
+void Text2SampleAudioProcessor::loadSample(int soundIndex, Sample::Ptr sample)
+{
     jassert(juce::isPositiveAndBelow(soundIndex, _synth.getNumSounds()));
 
-    if (auto sound = dynamic_cast<Sound *>(_synth.getSound(soundIndex).get()))
+    if (auto sound = dynamic_cast<Sound*>(_synth.getSound(soundIndex).get()))
         sound->setSample(sample);
 }
 
-void Text2SampleAudioProcessor::saveSample(int soundIndex, const juce::File &file) {
+void Text2SampleAudioProcessor::saveSample(int soundIndex, const juce::File& file)
+{
     jassert(juce::isPositiveAndBelow(soundIndex, _synth.getNumSounds()));
 
-    if (auto sound = dynamic_cast<Sound *>(_synth.getSound(soundIndex).get())) {
+    if (auto sound = dynamic_cast<Sound*>(_synth.getSound(soundIndex).get()))
+    {
         auto sample = sound->getSample();
 
         if (sample != nullptr)
@@ -96,9 +113,10 @@ void Text2SampleAudioProcessor::saveSample(int soundIndex, const juce::File &fil
     }
 }
 
-void Text2SampleAudioProcessor::loadSampleFromFile(int soundIndex, const juce::File &file) {
+void Text2SampleAudioProcessor::loadSampleFromFile(int soundIndex, const juce::File& file)
+{
     auto [data, fs] = Utils::readWavFile(file);
-    loadSample(soundIndex, new Sample{std::move(data), fs});
+    loadSample(soundIndex, new Sample{ std::move(data), fs });
 }
 
 void Text2SampleAudioProcessor::generateSample(int soundIndex) {
@@ -106,48 +124,59 @@ void Text2SampleAudioProcessor::generateSample(int soundIndex) {
     renderCRASHSample(&drum);
     loadSample(soundIndex, drum.sample);
 }
-
-const juce::String Text2SampleAudioProcessor::getName() const {
+const juce::String Text2SampleAudioProcessor::getName() const
+{
     return JucePlugin_Name;
 }
 
-bool Text2SampleAudioProcessor::acceptsMidi() const {
+bool Text2SampleAudioProcessor::acceptsMidi() const
+{
     return true;
 }
 
-bool Text2SampleAudioProcessor::producesMidi() const {
+bool Text2SampleAudioProcessor::producesMidi() const
+{
     return false;
 }
 
-bool Text2SampleAudioProcessor::isMidiEffect() const {
+bool Text2SampleAudioProcessor::isMidiEffect() const
+{
     return false;
 }
 
-double Text2SampleAudioProcessor::getTailLengthSeconds() const {
+double Text2SampleAudioProcessor::getTailLengthSeconds() const
+{
     return 0.0;
 }
 
-int Text2SampleAudioProcessor::getNumPrograms() {
+int Text2SampleAudioProcessor::getNumPrograms()
+{
     return 1;
 }
 
-int Text2SampleAudioProcessor::getCurrentProgram() {
+int Text2SampleAudioProcessor::getCurrentProgram()
+{
     return 0;
 }
 
-void Text2SampleAudioProcessor::setCurrentProgram(int) {
+void Text2SampleAudioProcessor::setCurrentProgram(int)
+{
 }
 
-const juce::String Text2SampleAudioProcessor::getProgramName(int) {
+const juce::String Text2SampleAudioProcessor::getProgramName(int)
+{
     return {};
 }
 
-void Text2SampleAudioProcessor::changeProgramName(int, const juce::String &) {
+void Text2SampleAudioProcessor::changeProgramName(int, const juce::String&)
+{
 }
 
-juce::AudioProcessorValueTreeState::ParameterLayout Text2SampleAudioProcessor::createParameterLayout() {
+juce::AudioProcessorValueTreeState::ParameterLayout Text2SampleAudioProcessor::createParameterLayout()
+{
     // Makes the list of parameters easier to read/edit
-    struct ParameterDefinition {
+    struct ParameterDefinition
+    {
         juce::String id;
         juce::String name;
         juce::NormalisableRange<float> range;
@@ -155,19 +184,21 @@ juce::AudioProcessorValueTreeState::ParameterLayout Text2SampleAudioProcessor::c
     };
 
     std::vector<ParameterDefinition> definitions = {
-            {"Gain", "Gain", {0.0f, 1.0f}, 0.5f}};
+        { "Gain", "Gain", { 0.0f, 1.0f }, 0.5f }
+    };
 
     std::vector<std::unique_ptr<juce::AudioParameterFloat>> params;
-    for (auto &d: definitions)
-        params.push_back(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID{d.id, 1},
-                                                                     d.name,
-                                                                     d.range,
+    for (auto& d : definitions)
+        params.push_back(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID{ d.id, 1 }, 
+                                                                     d.name, 
+                                                                     d.range, 
                                                                      d.defaultValue));
 
-    return {params.begin(), params.end()};
+    return { params.begin(), params.end() };
 }
 
-void Text2SampleAudioProcessor::renderCRASHSample(Drum *d) {
+void Text2SampleAudioProcessor::renderCRASHSample(Drum *d)
+{
     juce::AudioBuffer<float> data;
     size_t classification = 0;
     float confidence = 0;
@@ -175,14 +206,13 @@ void Text2SampleAudioProcessor::renderCRASHSample(Drum *d) {
     unetModelInference.process(data.getWritePointer(0));
     classifierModelInference.process(data.getReadPointer(0), &classification, &confidence);
     // 0 = Kick, 1 = Hat, 2 = Snare
-//    std::cout << "Classification: " << classification << " Confidence: " << confidence << std::endl;
     auto drumType = static_cast<DrumClass>(classification);
     d->sample = new Sample{std::move(data), UnetModelInference::sampleRate};
     d->drumType = drumType;
     d->confidence = confidence;
+}
 
-};
-
-juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter() {
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
     return new Text2SampleAudioProcessor();
 }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -6,71 +6,55 @@
 #include "UnetModelInference.h"
 #include "ClassifierModelInference.h"
 
-struct Limiter {
-    Limiter(juce::AudioBuffer<float> &data) {
+struct Limiter
+{
+    Limiter(juce::AudioBuffer<float>& data)
+    {
         for (int i = 0; i < data.getNumSamples(); i++)
             for (int j = 0; j < data.getNumChannels(); j++)
                 data.setSample(j, i, juce::jlimit(-1.0f, 1.0f, data.getSample(j, i)));
     }
 };
 
-class Text2SampleAudioProcessor : public juce::AudioProcessor {
+class Text2SampleAudioProcessor : public juce::AudioProcessor
+{
 public:
     const int numSounds = 12;
     const int numVoices = 8;
     const int baseMidiNote = 60;
 
     Text2SampleAudioProcessor();
-
     ~Text2SampleAudioProcessor() override;
 
-    bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
-
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
     void prepareToPlay(double sampleRate, int samplesPerBlock) override;
-
-    void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
-
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
     void releaseResources() override;
 
-    void getStateInformation(juce::MemoryBlock &destData) override;
-
-    void setStateInformation(const void *data, int sizeInBytes) override;
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
 
     bool hasEditor() const override;
-
-    juce::AudioProcessorEditor *createEditor() override;
-
-    juce::MidiKeyboardState &getMidiKeyboardState();
-
-    SoundWithParameters *getSound(int soundIndex);
+    juce::AudioProcessorEditor* createEditor() override;
+    juce::MidiKeyboardState& getMidiKeyboardState();
+    SoundWithParameters* getSound(int soundIndex);
 
     void loadSample(int soundIndex, Sample::Ptr sample);
+    void saveSample(int soundIndex, const juce::File& file);
 
-    void saveSample(int soundIndex, const juce::File &file);
-
-    void loadSampleFromFile(int soundIndex, const juce::File &file);
-
+    void loadSampleFromFile(int soundIndex, const juce::File& file);
     void generateSample(int soundIndex);
 
     const juce::String getName() const override;
-
     bool acceptsMidi() const override;
-
     bool producesMidi() const override;
-
     bool isMidiEffect() const override;
-
     double getTailLengthSeconds() const override;
-
     int getNumPrograms() override;
-
     int getCurrentProgram() override;
-
     void setCurrentProgram(int) override;
-
     const juce::String getProgramName(int) override;
-
-    void changeProgramName(int, const juce::String &) override;
+    void changeProgramName(int, const juce::String&) override;
 
 private:
     enum DrumClass {
@@ -83,20 +67,18 @@ private:
     };
 
     void renderCRASHSample(Drum *d);
-
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 
     juce::AudioProcessorValueTreeState _parameters;
 
     juce::Synthesiser _synth;
-    std::vector<SoundWithParameters *> _sounds;
-    std::vector<Voice *> _voices;
+    std::vector<SoundWithParameters*> _sounds;
+    std::vector<Voice*> _voices;
 
     UnetModelInference unetModelInference;
     ClassifierModelInference classifierModelInference;
 
     juce::MidiKeyboardState _midiState;
-
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Text2SampleAudioProcessor)
 };
